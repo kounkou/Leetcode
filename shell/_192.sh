1b5ae52a9df3c4f908b7fa04fb9584546993fca4
Godfrain Jacques <gkounkou@gmail.com>
2019-07-09 00:14:23 -0700

    Update _192.sh

--- shell/_192.sh
+++ shell/_192.sh
@@ -23,3 +23,7 @@
 
 # Read from the file words.txt and output the word frequency list to stdout.
 awk '{for(i=1;i<=NF;i++) a[$i]++} END {for(k in a) print k,a[k]}' words.txt | sort -k2 -nr
+
+# We use awk with its C syntax to create a map
+# then we increment the map corresponding element
+# then at the end we display the count for each element
